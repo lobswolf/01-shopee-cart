a4c068920406b5e1d9be0fd4edd61cc6a3d358dd
lobswolf <hugoandrad@msn.com>
2024-12-22 20:26:21 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import * as cartServices from "./services/cart.js"
+import createItem from "./services/item.js";
+
+const myCart = [];
+const myWhishList = [];
+
+console.log("Welcome to the your shopee cart!");
+
+const item1 = await createItem("hotwheels ferrari", 20.99, 1);
+const item2 = await createItem("hotwheels lamborguini", 39.99, 3);
+
+//Adicionei dois itens ao carrinho
+await cartServices.addItem(myCart, item1);
+await cartServices.addItem(myCart, item2);
+
+//Deletei dois itens do carrinho
+await cartServices.deleteItem(myCart, item2.name);
+await cartServices.deleteItem(myCart, item1.name);
+
+console.log("Shopee Cart TOTAL IS:");
+await cartServices.calculateTotal(myCart);
